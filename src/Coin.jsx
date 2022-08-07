070b00ca67f3d9e1d1c615e7cf3b44e1646320ee
Pravin kolate <88478158+PKwebTech@users.noreply.github.com>
2022-08-07 11:49:42 +0530

    REsponsive

--- src/Coin.jsx
+++ src/Coin.jsx
@@ -3,14 +3,14 @@ import './Coin.css'
 
 const Coin = ({image,name,price,pricechange,marketcap}) => {
     return (
-        <div className="coin-container">
-            <div className="coin-row">
-                <div className="coin">
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-4 coin col-sm-6 text-center">
                     <img src={image} alt="crypto" />
                     <h1>{name}</h1>
                     <p className="coin-symbol"></p>
                 </div>
-                <div className="coin-data">
+                <div className="col-md-4 col-sm-6 text-center">
                     <p className="coin-price">Rs.{price}</p>
                    
                     {pricechange<0 ? (
@@ -19,10 +19,14 @@ const Coin = ({image,name,price,pricechange,marketcap}) => {
                         <p className="coin-percent green">{pricechange.toFixed(2)}%</p>
                     )
                 }
+                
+                </div>
+                <div className='col-md-4 d-none d-md-block text-center'>
                 <p className="coin-marketcap">
                     Mkt Cap: Rs.{marketcap.toLocaleString()}
                 </p>
                 </div>
+                <hr className='hr'/>
             </div>
             
         </div>
